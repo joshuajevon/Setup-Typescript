881ae88b7021de27b085636329474cf1f9247e2a
joshuajevon <joshuajevon05@gmail.com>
2022-11-14 14:03:06 +0700

    fixing

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -3,9 +3,7 @@ import React from "react";
 const navbar = () => {
     return (
         <div>
-            <a href="/healthcare.html">Healthcare</a>
-            <a href="#">Cosmetic</a>
-            <a href="#">Misc</a>
+            
         </div>
     );
   };
